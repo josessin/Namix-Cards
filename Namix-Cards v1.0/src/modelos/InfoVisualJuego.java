e530eaec6c02cf7637ffe11cda5226a2efa61afc
Jose <Jose@DESKTOP-GF0J168>
2017-06-17 18:51:36 -0300

    Nueva clase: InfoVisualJuego para actualizar la pnatalla

---
+++ Namix-Cards v1.0/src/modelos/InfoVisualJuego.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jose
+ */
+public class InfoVisualJuego {
+    
+    private ArrayList<Carta> cartasJugadorMano;
+    private ArrayList<Carta> cartasJugadorTablero;
+    private ArrayList<Carta> cartasPCTablero;
+    private int vidasJugador;
+    private int vidasPC;
+    private int manaTotalJugador;
+    private int manaDispJugador;
+    private int manaTotalPC;
+    private int manaDispPC;
+
+    public InfoVisualJuego(){
+        
+    }
+    
+    
+    public ArrayList<Carta> getCartasJugadorMano() {
+        return cartasJugadorMano;
+    }
+
+    public void setCartasJugadorMano(ArrayList<Carta> cartasJugadorMano) {
+        this.cartasJugadorMano = cartasJugadorMano;
+    }
+
+    public ArrayList<Carta> getCartasJugadorTablero() {
+        return cartasJugadorTablero;
+    }
+
+    public void setCartasJugadorTablero(ArrayList<Carta> cartasJugadorTablero) {
+        this.cartasJugadorTablero = cartasJugadorTablero;
+    }
+
+    public ArrayList<Carta> getCartasPCTablero() {
+        return cartasPCTablero;
+    }
+
+    public void setCartasPCTablero(ArrayList<Carta> cartasPCTablero) {
+        this.cartasPCTablero = cartasPCTablero;
+    }
+
+    public int getVidasJugador() {
+        return vidasJugador;
+    }
+
+    public void setVidasJugador(int vidasJugador) {
+        this.vidasJugador = vidasJugador;
+    }
+
+    public int getVidasPC() {
+        return vidasPC;
+    }
+
+    public void setVidasPC(int vidasPC) {
+        this.vidasPC = vidasPC;
+    }
+
+    public int getManaTotalJugador() {
+        return manaTotalJugador;
+    }
+
+    public void setManaTotalJugador(int manaTotalJugador) {
+        this.manaTotalJugador = manaTotalJugador;
+    }
+
+    public int getManaDispJugador() {
+        return manaDispJugador;
+    }
+
+    public void setManaDispJugador(int manaDispJugador) {
+        this.manaDispJugador = manaDispJugador;
+    }
+
+    public int getManaTotalPC() {
+        return manaTotalPC;
+    }
+
+    public void setManaTotalPC(int manaTotalPC) {
+        this.manaTotalPC = manaTotalPC;
+    }
+
+    public int getManaDispPC() {
+        return manaDispPC;
+    }
+
+    public void setManaDispPC(int manaDispPC) {
+        this.manaDispPC = manaDispPC;
+    }
+    
+    
+    
+}
